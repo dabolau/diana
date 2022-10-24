7bbbd68e376750478e5a1e22a7cce0f3afe909b5
dabolau <dabolau@qq.com>
2022-10-24 15:29:55 +0800

    播放视频不息屏

--- lib/app/modules/play/views/play_view.dart
+++ lib/app/modules/play/views/play_view.dart
@@ -19,6 +19,7 @@ class PlayView extends GetView<PlayController> {
         centerTitle: true,
         elevation: 0,
       ),
+
       // floatingActionButton: FloatingActionButton(
       //   onPressed: () {
       //     /// 获取视频信息
@@ -32,6 +33,7 @@ class PlayView extends GetView<PlayController> {
         init: PlayController(),
         builder: (controller) {
           return Container(
+            // margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
             child: Column(
               children: [
                 //////
@@ -64,21 +66,33 @@ class PlayView extends GetView<PlayController> {
                     //     child: Text('广告'),
                     //   ),
                     // ),
+
+                    /// 返回按钮
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      // right: 0,
+                      // bottom: 0,
+                      child: IconButton(
+                        onPressed: (() {
+                          /// 返回
+                          Get.back();
+                        }),
+                        icon: Icon(
+                          Icons.keyboard_arrow_left,
+                          color: Colors.white,
+                          size: 35,
+                        ),
+                      ),
+                    ),
                   ]),
                 ),
 
-                // //////
-                // /// 间隔
-                // //////
-                // SizedBox(
-                //   height: 10,
-                // ),
-
                 //////
-                /// 标签信息，广告，剧集，简介
+                /// 标签信息，剧集，简介
                 //////
                 DefaultTabController(
-                  length: 3,
+                  length: 2,
                   child: Column(
                     children: [
                       //////
@@ -106,9 +120,6 @@ class PlayView extends GetView<PlayController> {
                           indicatorSize: TabBarIndicatorSize.label, // 长度
                           tabs: [
                             Tab(
-                              text: '广告',
-                            ),
-                            Tab(
                               text: '剧集',
                             ),
                             Tab(
@@ -132,35 +143,34 @@ class PlayView extends GetView<PlayController> {
                         child: TabBarView(
                           children: [
                             //////
-                            /// 广告内容
-                            //////
-
-                            Container(
-                              child: Center(
-                                child: Text('广告'),
-                              ),
-                            ),
-
-                            //////
                             /// 剧集内容
                             //////
                             Container(
-                              margin: EdgeInsets.only(
-                                  left: 10, top: 10, right: 10, bottom: 10),
                               child: ListView(
+                                shrinkWrap:true,
+                                physics:NeverScrollableScrollPhysics(),
                                 children: [
                                   //////
                                   /// 选集导航栏
                                   //////
                                   Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 10,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    // color: Colors.red,
                                     child: Row(
                                       children: [
                                         Text('选集'),
                                         Spacer(),
                                         IconButton(
-                                            onPressed: (() {}),
-                                            icon: Icon(
-                                                Icons.keyboard_arrow_right)),
+                                          onPressed: (() {}),
+                                          icon:
+                                              Icon(Icons.keyboard_arrow_right),
+                                        ),
+                                        // Icon(Icons.keyboard_arrow_right),
                                       ],
                                     ),
                                   ),
@@ -168,6 +178,12 @@ class PlayView extends GetView<PlayController> {
                                   /// 选集按钮
                                   //////
                                   Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
                                     // color: Colors.black,
                                     height: 40,
                                     child: ListView.builder(
@@ -182,11 +198,6 @@ class PlayView extends GetView<PlayController> {
                                               : EdgeInsets.only(left: 10),
                                           child: TextButton(
                                             onPressed: () {
-                                              print(
-                                                  '${controller.videoArguments["URLs"][index]["Name"]}');
-                                              print(
-                                                  '${controller.videoArguments["URLs"][index]["URL"]}');
-
                                               /// 赋值给控制器
                                               controller.name.value = controller
                                                       .videoArguments["URLs"]
@@ -245,38 +256,120 @@ class PlayView extends GetView<PlayController> {
                             /// 简介内容
                             //////
                             Container(
-                              margin: EdgeInsets.only(
-                                  left: 10, top: 10, right: 10, bottom: 10),
                               child: ListView(
+                                shrinkWrap:true,
+                                physics:NeverScrollableScrollPhysics(),
                                 children: [
-                                  Text(
-                                      '片名：${controller.videoArguments["Name"]}'),
-                                  SizedBox(
-                                    height: 10,
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 10,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '片名：${controller.videoArguments["Name"]}'),
                                   ),
-                                  Text(
-                                      '简介：${controller.videoArguments["Description"]}'),
-                                  SizedBox(
-                                    height: 10,
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '简介：${controller.videoArguments["Description"]}'),
                                   ),
-                                  Text(
-                                      '导演：${controller.videoArguments["Director"]}'),
-                                  Text(
-                                      '演员：${controller.videoArguments["Actor"]}'),
-                                  Text(
-                                      '类别：${controller.videoArguments["Category"]}'),
-                                  Text(
-                                      '类型：${controller.videoArguments["Type"]}'),
-                                  Text(
-                                      '地区：${controller.videoArguments["Area"]}'),
-                                  Text(
-                                      '语言：${controller.videoArguments["Language"]}'),
-                                  Text(
-                                      '年份：${controller.videoArguments["Year"]}'),
-                                  Text(
-                                      '评分：${controller.videoArguments["Score"]}'),
-                                  Text(
-                                      '状态：${controller.videoArguments["Status"]}'),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '导演：${controller.videoArguments["Director"]}'),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '演员：${controller.videoArguments["Actor"]}'),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '类别：${controller.videoArguments["Category"]}'),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '类型：${controller.videoArguments["Type"]}'),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '地区：${controller.videoArguments["Area"]}'),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '语言：${controller.videoArguments["Language"]}'),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '年份：${controller.videoArguments["Year"]}'),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '评分：${controller.videoArguments["Score"]}'),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      left: 10,
+                                      top: 0,
+                                      right: 10,
+                                      bottom: 10,
+                                    ),
+                                    child: Text(
+                                        '状态：${controller.videoArguments["Status"]}'),
+                                  ),
                                 ],
                               ),
                             ),
@@ -286,13 +379,6 @@ class PlayView extends GetView<PlayController> {
                     ],
                   ),
                 ),
-
-                //////
-                /// 间隔
-                //////
-                // SizedBox(
-                //   height: 10,
-                // ),
               ],
             ),
           );
